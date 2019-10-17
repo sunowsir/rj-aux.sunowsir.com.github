f012f3d49fba1e218d57081402182004b98bfab9
sunowsir <sunow.wang@gmail.com>
2019-10-17 23:08:17 +0800

    update some code && add some script in build dir

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,14 @@ DWIDGET_USE_NAMESPACE
 int main(int argc, char *argv[])
 {
 
-    DApplication::loadDXcbPlugin();  //让bar处在标题栏中
+    DApplication::loadDXcbPlugin();
     DApplication a(argc, argv);
 
     a.setAttribute(Qt::AA_UseHighDpiPixmaps);
     a.loadTranslator();
     a.setOrganizationName("deepin");
-    a.setApplicationVersion(DApplication::buildVersion("1.0"));
-    //a.setApplicationAcknowledgementPage("https://你的网站");
-    // a.setProductIcon(QIcon(":/image/resource/logo.png"));  //设置Logo
-    //a.setProductName("DtkDemo");
-    a.setApplicationName("锐捷linux有线认证GUI辅助"); //只有在这儿修改窗口标题才有效
+    a.setApplicationVersion(DApplication::buildVersion("2.3"));
+    a.setApplicationName("锐捷linux有线认证GUI辅助");
 
     a.setWindowIcon(QIcon(":/image/resource/icon.png"));
 
