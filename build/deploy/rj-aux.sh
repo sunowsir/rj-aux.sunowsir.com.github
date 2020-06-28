5097685eaa867ab394de9df00ece3352b4203230
sunowsir <sunow.wang@gmail.com>
2020-06-28 22:26:49 +0800

    deploy test

---
+++ build/deploy/rj-aux.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#	* File     : rj-aux.sh
+#	* Author   : sunowsir
+#	* Mail     : sunowsir@163.com
+#	* Github   : github.com/sunowsir
+#	* Creation : Sun 28 Jun 2020 10:10:06 PM CST
+
+appname="$(basename "$0" | sed s,\.sh,,)"  
+dirname="$(dirname "$0")"  
+tmp="${dirname#?}"  
+
+if [ "${dirname%$tmp}" != "/" ]; then  
+	dirname="$PWD"/"$dirname"  
+fi  
+
+LD_LIBRARY_PATH="$dirname"  
+export LD_LIBRARY_PATH  
+
+"$dirname"/"$appname" "$@"
