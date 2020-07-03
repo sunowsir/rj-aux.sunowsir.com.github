9df773330a67e936f71b7514478a16741ac9cadb
sunowsir <sunow.wang@gmail.com>
2020-07-03 19:51:20 +0800

    update : core

--- build/moc_traywidget.cpp
+++ build/moc_traywidget.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_TrayWidget_t {
-    QByteArrayData data[1];
-    char stringdata0[11];
+    QByteArrayData data[4];
+    char stringdata0[33];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,10 +32,13 @@ struct qt_meta_stringdata_TrayWidget_t {
     )
 static const qt_meta_stringdata_TrayWidget_t qt_meta_stringdata_TrayWidget = {
     {
-QT_MOC_LITERAL(0, 0, 10) // "TrayWidget"
+QT_MOC_LITERAL(0, 0, 10), // "TrayWidget"
+QT_MOC_LITERAL(1, 11, 9), // "load_core"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 10) // "show_about"
 
     },
-    "TrayWidget"
+    "TrayWidget\0load_core\0\0show_about"
 };
 #undef QT_MOC_LITERAL
 
@@ -45,21 +48,35 @@ static const uint qt_meta_data_TrayWidget[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       0,    0, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   24,    2, 0x0a /* Public */,
+       3,    0,   25,    2, 0x0a /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
        0        // eod
 };
 
 void TrayWidget::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
-    Q_UNUSED(_o);
-    Q_UNUSED(_id);
-    Q_UNUSED(_c);
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<TrayWidget *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->load_core(); break;
+        case 1: _t->show_about(); break;
+        default: ;
+        }
+    }
     Q_UNUSED(_a);
 }
 
@@ -89,6 +106,17 @@ void *TrayWidget::qt_metacast(const char *_clname)
 int TrayWidget::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QWidget::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 2;
+    }
     return _id;
 }
 QT_WARNING_POP
